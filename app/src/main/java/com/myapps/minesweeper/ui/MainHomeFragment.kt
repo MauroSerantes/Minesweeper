0ed62e24e2b652c41e3d1262afae9950e07b468a
mauro <mauromatiasserantes@gmail.com>
2024-05-19 02:21:05 -0300

    modification in selection difficulty ui (use of dialog)

--- app/src/main/java/com/myapps/minesweeper/ui/MainHomeFragment.kt
+++ app/src/main/java/com/myapps/minesweeper/ui/MainHomeFragment.kt
@@ -1,11 +1,13 @@
 package com.myapps.minesweeper.ui
 
-import androidx.fragment.app.Fragment
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.myapps.minesweeper.R
 import com.myapps.minesweeper.databinding.FragmentMainHomeBinding
@@ -27,19 +29,7 @@ class MainHomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initButtonsForSelectionDifficultyMenu()
-        initSelectionDifficultyButtons()
-        setGradientColorTextForPresentationGame()
-    }
 
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-
-    private fun setGradientColorTextForPresentationGame(){
         setTextViewColor(binding.minesweeperTittleText,
             intArrayOf(
                 ContextCompat.getColor(requireContext(), R.color.yellow),
@@ -49,67 +39,77 @@ class MainHomeFragment : Fragment() {
                 ContextCompat.getColor(requireContext(),R.color.blue),
                 ContextCompat.getColor(requireContext(),R.color.green))
         )
-        setTextViewColor(binding.tvBeginner,
-            intArrayOf(ContextCompat.getColor(requireContext(),R.color.yellow),
-                ContextCompat.getColor(requireContext(),R.color.orange),
-                ContextCompat.getColor(requireContext(),R.color.red),
-                ContextCompat.getColor(requireContext(),R.color.purple),
-                ContextCompat.getColor(requireContext(),R.color.blue),
-                ContextCompat.getColor(requireContext(),R.color.green))
-        )
-        setTextViewColor(binding.tvMedium,
-            intArrayOf(ContextCompat.getColor(requireContext(),R.color.yellow),
-                ContextCompat.getColor(requireContext(),R.color.orange),
-                ContextCompat.getColor(requireContext(),R.color.red),
-                ContextCompat.getColor(requireContext(),R.color.purple),
-                ContextCompat.getColor(requireContext(),R.color.blue),
-                ContextCompat.getColor(requireContext(),R.color.green))
-        )
-        setTextViewColor(binding.tvAdvanced,
-            intArrayOf(ContextCompat.getColor(requireContext(),R.color.yellow),
-                ContextCompat.getColor(requireContext(),R.color.orange),
-                ContextCompat.getColor(requireContext(),R.color.red),
-                ContextCompat.getColor(requireContext(),R.color.purple),
-                ContextCompat.getColor(requireContext(),R.color.blue),
-                ContextCompat.getColor(requireContext(),R.color.green))
-        )
-    }
 
-    private fun initButtonsForSelectionDifficultyMenu(){
-        binding.apply {
-            playButton.setOnClickListener{
-                binding.selectionDifficultyLayout.visibility = View.VISIBLE
-                binding.playButton.isEnabled = false
-            }
+        binding.playButton.setOnClickListener {
+            val dialog = Dialog(requireContext())
+            dialog.setContentView(R.layout.difficulty_selection)
 
-            backButton.setOnClickListener{
-                binding.selectionDifficultyLayout.visibility = View.GONE
-                binding.playButton.isEnabled = true
-            }
-        }
-    }
-
-    private fun initSelectionDifficultyButtons(){
-        binding.apply {
-            beginnerButton.setOnClickListener {
-                val gameDetails = "8,8,10" //the first number is for rows, the second for columns and the last
-                //is for the amount of mines
-                val direction = MainHomeFragmentDirections.actionMainHomeFragmentToGameFragment(gameDetails)
-                findNavController().navigate(direction)
-            }
-            mediumButton.setOnClickListener {
+            dialog.findViewById<View>(R.id.beginnerButton)
+                .setOnClickListener {
+                    val gameDetails =
+                        "8,8,10"
+                    val direction =
+                        MainHomeFragmentDirections.actionMainHomeFragmentToGameFragment(gameDetails)
+                    findNavController().navigate(direction)
+                    dialog.dismiss()
+                }
+            dialog.findViewById<View>(R.id.mediumButton).setOnClickListener {
                 val gameDetails = "32,8,40"
-                val direction = MainHomeFragmentDirections.actionMainHomeFragmentToGameFragment(gameDetails)
+                val direction =
+                    MainHomeFragmentDirections.actionMainHomeFragmentToGameFragment(gameDetails)
                 findNavController().navigate(direction)
+                dialog.dismiss()
             }
-            advancedButton.setOnClickListener {
+
+            dialog.findViewById<View>(R.id.advancedButton).setOnClickListener {
                 val gameDetails = "60,8,99"
                 val direction = MainHomeFragmentDirections.actionMainHomeFragmentToGameFragment(gameDetails)
                 findNavController().navigate(direction)
+                dialog.dismiss()
             }
+
+            setTextViewColor(dialog.findViewById(R.id.tvBeginner),
+                intArrayOf(ContextCompat.getColor(requireContext(),R.color.yellow),
+                    ContextCompat.getColor(requireContext(),R.color.orange),
+                    ContextCompat.getColor(requireContext(),R.color.red),
+                    ContextCompat.getColor(requireContext(),R.color.purple),
+                    ContextCompat.getColor(requireContext(),R.color.blue),
+                    ContextCompat.getColor(requireContext(),R.color.green))
+            )
+            setTextViewColor(dialog.findViewById(R.id.tvMedium),
+                intArrayOf(ContextCompat.getColor(requireContext(),R.color.yellow),
+                    ContextCompat.getColor(requireContext(),R.color.orange),
+                    ContextCompat.getColor(requireContext(),R.color.red),
+                    ContextCompat.getColor(requireContext(),R.color.purple),
+                    ContextCompat.getColor(requireContext(),R.color.blue),
+                    ContextCompat.getColor(requireContext(),R.color.green))
+            )
+            setTextViewColor(dialog.findViewById(R.id.tvAdvanced),
+                intArrayOf(ContextCompat.getColor(requireContext(),R.color.yellow),
+                    ContextCompat.getColor(requireContext(),R.color.orange),
+                    ContextCompat.getColor(requireContext(),R.color.red),
+                    ContextCompat.getColor(requireContext(),R.color.purple),
+                    ContextCompat.getColor(requireContext(),R.color.blue),
+                    ContextCompat.getColor(requireContext(),R.color.green))
+            )
+
+
+
+            dialog.findViewById<View>(R.id.dialog_button).setOnClickListener{
+                dialog.dismiss()
+            }
+            dialog.show()
         }
     }
 
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 
 }
+
+
+
